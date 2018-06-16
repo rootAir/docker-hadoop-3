651f1e75b5677cbf454ba4bc6b333ccba1730e54
Luiz <junqueira.luizcarlo@gmail.com>
2018-06-16 09:11:52 -0300

    ajuste hadoop 3.1 in docker

---
+++ start.sh
@@ -0,0 +1,7 @@
+docker build -t hadoop3 .
+
+docker ps -qa | xargs docker rm --force
+
+docker run --hostname=hadoop3 -p 8088:8088 -p 9870:9870 -p 9864:9864 -p 19888:19888 -p 8042:8042 -p 8888:8888 --name hadoop3 -d hadoop3
+
+docker exec -it hadoop3 bash
